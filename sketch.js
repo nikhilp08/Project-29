d35f414a2de1b98dedd0a5ed6d59f3e47d668099
nikhilp08 <69642926+nikhilp08@users.noreply.github.com>
2020-09-30 22:19:50 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+
+var block1,block2,block3,block4,block5,block6,block7,block8,block9;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  
+  base = new Ground(390,265,200,20)
+
+  block1 = new Block(330,235,30,40);
+  block2 = new Block(360,235,30,40);
+  block3 = new Block(390,235,30,40);
+  block4 = new Block(420,235,30,40);
+  block5 = new Block(450,235,30,40);
+
+  block6 = new Block(360,195,30,40);
+  block7 = new Block(390,195,30,40);
+  block8 = new Block(420,195,30,40);
+
+  block9 = new Block(390,155,30,40);
+
+  polygon = Bodies.circle(50,200,20);
+  World.add(world,polygon);
+
+  
+  slingshot = new SlingShot(this.polygon,{x:100,y:200});
+
+}
+
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(polygon.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  slingshot.fly();
+}
